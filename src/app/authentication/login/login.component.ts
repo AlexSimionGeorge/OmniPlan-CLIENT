1feef662dfae1db186cc628647791a56c66ba08e
NoNameMB10K <al3xs1m10n@gmail.com>
2024-09-20 12:19:42 +0300

    3-Added login request model

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -4,6 +4,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButton} from "@angular/material/button";
+import {LoginRequestModel} from "../models/login-request.model";
 
 @Component({
   selector: 'app-authentication',
@@ -19,12 +20,16 @@ import {MatButton} from "@angular/material/button";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  loginFormGroup: FormGroup<{name: FormControl, password: FormControl}> = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+  loginFormGroup: FormGroup<{identifier: FormControl, password: FormControl}> = new FormGroup({
+    identifier: new FormControl('', [Validators.required, Validators.maxLength(255)]),
     password: new FormControl('', [Validators.required, Validators.maxLength(255), Validators.minLength(8)]),
   });
 
   save() {
-    console.log("save");
+    const loginRequest: LoginRequestModel = {
+      identifier: this.loginFormGroup.controls.identifier.value,
+      password: this.loginFormGroup.controls.password.value,
+    };
+    console.log(loginRequest);
   }
 }
