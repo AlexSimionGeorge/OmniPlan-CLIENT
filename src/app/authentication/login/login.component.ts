469e7a4da5edf26a325f3047771ce76c28dff992
NoNameMB10K <al3xs1m10n@gmail.com>
2024-09-20 11:52:34 +0300

    3-Removed .vs added envs

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatButton} from "@angular/material/button";
 
 @Component({
@@ -20,8 +20,8 @@ import {MatButton} from "@angular/material/button";
 })
 export class LoginComponent {
   loginFormGroup: FormGroup<{name: FormControl, password: FormControl}> = new FormGroup({
-    name: new FormControl('', {nonNullable: true}),
-    password: new FormControl('', {nonNullable: true}),
+    name: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+    password: new FormControl('', [Validators.required, Validators.maxLength(255), Validators.minLength(8)]),
   });
 
   save() {
